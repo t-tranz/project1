ca53bb057cff41320c30c73ee0efeb670f3f3d30
Maksat <maksat.kurbanov20@gmail.com>
2023-01-11 14:49:31 +0500

    added variable in object

--- js/script.js
+++ js/script.js
@@ -21,7 +21,6 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
-'use strict';
 // alert('Hello my friend!!!');
 
 // const result = confirm("Are you here ?");
@@ -30,4 +29,24 @@
 // const answer = +prompt("Вам есть 18?", "18");
 // console.log(typeof(answer));
 
-const answers = [];
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const a = prompt('Один из последних просмотренных фильмов?',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его?','');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
